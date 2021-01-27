3b333691bb38492ae9d5b87852cc8b564fc1d73e
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2021-01-28 03:13:39 +0900

    add available decorators

---
+++ src/posts/dto.ts
@@ -0,0 +1,13 @@
+export class CreatePostDto {
+  title: String;
+  description: String;
+}
+
+export class UpdatePostDto {
+  title: String;
+  description: String;
+}
+
+export class ListAllEntities {
+  
+}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Query, Param, Delete } from '@nestjs/common';
+import { CreatePostDto, UpdatePostDto, ListAllEntities } from './dto';
 
 @Controller('posts')
 export class PostsController {
-  @Get() // create a handler for a specific endpoint for HTTP requests.
-  findAll(): string {
-    return 'all posts';
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return 'Add a post';
   }
+
+  @Get() // create a handler for a specific endpoint for HTTP requests.
+  findAll(@Query() query: ListAllEntities): string {
+    return 'Get all posts';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Get #${id} post`
+  }
+
+  @Put(':id')
+  update(@Param('id') id:string, @Body() UpdatePostDto: UpdatePostDto) {
+    return `Update #${id} post`
+
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Delete #${id} post`
+  }
+}
